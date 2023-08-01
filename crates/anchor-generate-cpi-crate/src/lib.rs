10b6ff06a0c1ee6a70c37a5a406720b18799b9a3
alphaprime8 <alphaprime8888@gmail.com>
2023-08-01 21:19:03 +0800

    upgraded all crates

--- crates/anchor-generate-cpi-crate/src/lib.rs
+++ crates/anchor-generate-cpi-crate/src/lib.rs
@@ -14,30 +14,14 @@
 //!
 //! More examples can be found in the [examples/](https://github.com/saber-hq/anchor-gen/tree/master/examples) directory.
 
-use anchor_idl::GeneratorOptions;
 use syn::{parse_macro_input, LitStr};
+use anchor_idl_mod::GeneratorOptions;
 
 /// Generates an Anchor CPI crate from a JSON file.
 ///
 /// # Arguments
 ///
 /// * `input` - Path to a JSON IDL relative to the crate's the Cargo.toml.
-///
-/// # Examples
-///
-/// ```
-/// anchor_generate_cpi_crate::generate_cpi_crate!("../../examples/govern-cpi/idl.json");
-/// declare_id!("GjphYQcbP1m3FuDyCTUJf2mUMxKPE3j6feWU1rxvC7Ps");
-/// # fn main() -> Result<()> {
-/// let _my_governor = GovernanceParameters {
-///     quorum_votes: 0,
-///     timelock_delay_seconds: 0,
-///     voting_period: 0,
-///     voting_delay: 0,
-/// };
-/// #   Ok(())
-/// # }
-/// ```
 #[proc_macro]
 pub fn generate_cpi_crate(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let id_literal = parse_macro_input!(input as LitStr);
--- crates/anchor-generate-cpi-interface/src/lib.rs
+++ crates/anchor-generate-cpi-interface/src/lib.rs
@@ -14,10 +14,10 @@
 //!
 //! More examples can be found in the [examples/](https://github.com/saber-hq/anchor-gen/tree/master/examples) directory.
 
-use anchor_idl::GeneratorOptions;
 use darling::FromMeta;
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
+use anchor_idl_mod::GeneratorOptions;
 
 /// Generates an Anchor CPI crate from a JSON file.
 ///
@@ -25,21 +25,6 @@ use syn::parse_macro_input;
 ///
 /// * `idl_path` - Path to a JSON IDL relative to the crate's the Cargo.toml.
 ///
-/// # Examples
-///
-/// ```
-/// anchor_generate_cpi_interface::generate_cpi_interface!(idl_path = "../../examples/govern-cpi/idl.json");
-/// declare_id!("GjphYQcbP1m3FuDyCTUJf2mUMxKPE3j6feWU1rxvC7Ps");
-/// # fn main() -> Result<()> {
-/// let _my_governor = GovernanceParameters {
-///     quorum_votes: 0,
-///     timelock_delay_seconds: 0,
-///     voting_period: 0,
-///     voting_delay: 0,
-/// };
-/// #   Ok(())
-/// # }
-/// ```
 #[proc_macro]
 pub fn generate_cpi_interface(input: proc_macro::TokenStream) -> TokenStream {
     let attr_args = parse_macro_input!(input as syn::AttributeArgs);
